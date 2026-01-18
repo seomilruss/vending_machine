d46090504d704695e532596f4ca18e11871fd6ee
Cho Young Deok(조영덕) <54846678+seomilruss@users.noreply.github.com>
2026-01-18 11:29:56 +0900

    Add files via upload

---
+++ vending_machine.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+
+
+tea_rating_1_kor_name = "고급";
+tea_rating_1_eng_name = "Fine";
+
+tea_rating_2_kor_name = "일반";
+tea_rating_2_eng_name = "Regular";
+
+tea_rating_3_kor_name = "국산차";
+tea_rating_3_eng_name = "Domestic Tea";
+
+fine_coffe_1_kor_name = "블랙";
+fine_coffe_1_eng_name = "Black";
+fine_coffe_2_kor_name = "설탕";
+fine_coffe_2_eng_name = "Sugar";
+fine_coffe_3_kor_name = "크림";
+fine_coffe_3_eng_name = "Cream";
+fine_coffe_4_kor_name = "설탕크림";
+fine_coffe_4_eng_name = "Sugar Cream";
+
+fine_coffe_1_price = 500;
+fine_coffe_2_price = 500;
+fine_coffe_3_price = 500;
+fine_coffe_4_price = 500;
+
+regular_coffe_1_kor_name = "블랙";
+regular_coffe_1_eng_name = "Black";
+regular_coffe_2_kor_name = "설탕";
+regular_coffe_2_eng_name = "Sugar";
+regular_coffe_3_kor_name = "크림";
+regular_coffe_3_eng_name = "Cream";
+regular_coffe_4_kor_name = "설탕크림";
+regular_coffe_4_eng_name = "Sugar Cream";
+
+regular_coffe_1_price = 400;
+regular_coffe_2_price = 400;
+regular_coffe_3_price = 400;
+regular_coffe_4_price = 400;
+
+domestic_tea_1_kor_name = "율무차";
+domestic_tea_1_eng_name = "Adlay Tea";
+domestic_tea_2_kor_name="우유";
+domestic_tea_2_eng_name = "Milk";
+domestic_tea_3_kor_name="코코아";
+domestic_tea_3_eng_name = "Cocoa";
+domestic_tea_4_kor_name="밀크초코";
+domestic_tea_4_eng_name = "Milk Choco";
+
+domestic_tea_1_price = 300;
+domestic_tea_2_price = 300;
+domestic_tea_3_price = 300;
+domestic_tea_4_price = 300;
+
+print(
+        "[" +
+                tea_rating_1_kor_name + "(" + tea_rating_1_eng_name +  ")" +
+                " : " +
+                fine_coffe_1_kor_name + "(" + fine_coffe_1_eng_name + ") " + fine_coffe_1_price + " 원 " +
+                fine_coffe_2_kor_name + "(" + fine_coffe_2_eng_name + ") " + fine_coffe_2_price + " 원 " +
+                fine_coffe_3_kor_name + "(" + fine_coffe_3_eng_name + ") " + fine_coffe_3_price + " 원 " +
+                fine_coffe_4_kor_name + "(" + fine_coffe_4_eng_name + ") " + fine_coffe_4_price + " 원 " +
+        "]"
+);
+print(
+        "[" +
+                tea_rating_2_kor_name + "(" + tea_rating_2_eng_name +  ")" +
+                " : " +
+                regular_coffe_1_kor_name + "(" + regular_coffe_1_eng_name + ") " + regular_coffe_1_price + " 원 " +
+                regular_coffe_2_kor_name + "(" + regular_coffe_2_eng_name + ") " + regular_coffe_2_price + " 원 " +
+                regular_coffe_3_kor_name + "(" + regular_coffe_3_eng_name + ") " + regular_coffe_3_price + " 원 " +
+                regular_coffe_4_kor_name + "(" + regular_coffe_4_eng_name + ") " + regular_coffe_4_price + " 원 " +
+        "]"
+);
+print(
+        "[" +
+                tea_rating_3_kor_name + "(" + tea_rating_3_eng_name +  ")" +
+                " : " +
+                domestic_tea_1_kor_name + "(" + domestic_tea_1_eng_name + ") " + domestic_tea_1_price + " 원 " +
+                domestic_tea_2_kor_name + "(" + domestic_tea_2_eng_name + ") " + domestic_tea_2_price + " 원 " +
+                domestic_tea_3_kor_name + "(" + domestic_tea_3_eng_name + ") " + domestic_tea_3_price + " 원 " +
+                domestic_tea_4_kor_name + "(" + domestic_tea_4_eng_name + ") " + domestic_tea_4_price + " 원 " +
+        "]"
+);
