c902ec284441c87e2e1ab104540b643916b9583f
Cho Young Deok(조영덕) <54846678+seomilruss@users.noreply.github.com>
2026-01-20 06:28:35 +0900

    Update vending_machine_drink.sql

--- vending_machine_drink.sql
+++ vending_machine_drink.sql
@@ -1,7 +1,7 @@
 --	음료 전용 자판기
 CREATE TABLE vending_machine_drink (
-		drink_record INT NOT NULL AUTO_INCREMENT, -- (삭제가능 가변)순번
-		drink_id VARCHAR(5) NOT NULL PRIMARY KEY, -- 음료 식별자
+		drink_record INT NOT NULL, -- (삭제가능 가변)순번
+		drink_id VARCHAR(5) NOT NULL PRIMARY KEY AUTO_INCREMENT, -- 음료 식별자
 		drink_classification_eng VARCHAR(20) NOT NULL, -- 영문 음료 구분 
 		drink_classification_kor VARCHAR(20) NOT NULL, -- 한글 음료 구분
 		drink_name_eng VARCHAR(35), -- 영문 음료 이름
