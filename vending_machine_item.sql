a8ed8adeba6c8265c327eb1893b8f6013ecce66e
Cho Young Deok(조영덕) <54846678+seomilruss@users.noreply.github.com>
2026-01-20 06:31:55 +0900

    Update vending_machine_item.sql

--- vending_machine_item.sql
+++ vending_machine_item.sql
@@ -1,7 +1,7 @@
 --	상품(물품) 전용 자판기
 CREATE TABLE vending_machine_item (
-		item_record INT NOT NULL AUTO_INCREMENT, -- (삭제가능 가변)순번
-		item_id VARCHAR(5) NOT NULL PRIMARY KEY, -- 상품 식별자
+		item_record INT NOT NULL, -- (삭제가능 가변)순번
+		item_id VARCHAR(5) AUTO_INCREMENT NOT NULL PRIMARY KEY, -- 상품 식별자
 		item_classification_eng VARCHAR(20) NOT NULL, -- 영문 상품 구분 
 		item_classification_kor VARCHAR(20) NOT NULL, -- 한글 상품 구분
 		item_name_eng VARCHAR(35), -- 영문 상품 이름
