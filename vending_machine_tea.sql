7ae289c1d76cbf953527fa2b4f532ddb76749b63
Cho Young Deok(조영덕) <54846678+seomilruss@users.noreply.github.com>
2026-01-20 06:27:01 +0900

    Update vending_machine_tea.sql

--- vending_machine_tea.sql
+++ vending_machine_tea.sql
@@ -1,7 +1,7 @@
 --	차 전용 자판기
 CREATE TABLE vending_machine_tea (
-		tea_record INT NOT NULL AUTO_INCREMENT, -- (삭제가능 가변)순번
-		tea_id VARCHAR(5) NOT NULL PRIMARY KEY, -- 차 식별자
+		tea_record INT NOT NULL, -- (삭제가능 가변)순번
+		tea_id VARCHAR(5) NOT NULL PRIMARY KEY AUTO_INCREMENT, -- 차 식별자
 		tea_classification_eng VARCHAR(20) NOT NULL, -- 영문 차 구분 
 		tea_classification_kor VARCHAR(20) NOT NULL, -- 한글 차 구분
 		tea_name_eng VARCHAR(35), -- 영문 차 이름
